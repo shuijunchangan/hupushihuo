0f3efa1e8da222c57ef12f93d3d7620429043a55
shuijunchangan <381468656@qq.com>
2019-05-10 16:34:01 +0800

    初始化了两个文件

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,64 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+		<router-view/>
+    
+    <ul class="footer">
+    	<li>
+				<router-link to="/shouye" active-class="active" class-active><span class="iconfont">&#xe606;</span><span>首页</span></router-link>
+			</li>
+    	<li><router-link to="/youhui" active-class="active"><span class="iconfont">&#xe773;</span><span>优惠</span></router-link></li>
+    	<li><router-link to="/faxian" active-class="active"><span class="iconfont">&#xe629;</span><span>发现</span></router-link></li>
+    	<li><router-link to="/zhuangbei" active-class="active"><span class="iconfont">&#xe613;</span><span>装备</span></router-link></li>
+    	<li><router-link to="/gengduo" active-class="active"><span class="iconfont">&#xe64d;</span><span>更多</span></router-link></li>
+    </ul>
+    
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<style lang="scss" scoped>
+	@import "./assets/css/reset.css";
+	
+	@font-face {
+		font-family: 'iconfont';  /* project id 1182626 */
+		src: url('//at.alicdn.com/t/font_1182626_7j8hwqe4vy5.eot');
+		src: url('//at.alicdn.com/t/font_1182626_7j8hwqe4vy5.eot?#iefix') format('embedded-opentype'),
+		url('//at.alicdn.com/t/font_1182626_7j8hwqe4vy5.woff2') format('woff2'),
+		url('//at.alicdn.com/t/font_1182626_7j8hwqe4vy5.woff') format('woff'),
+		url('//at.alicdn.com/t/font_1182626_7j8hwqe4vy5.ttf') format('truetype'),
+		url('//at.alicdn.com/t/font_1182626_7j8hwqe4vy5.svg#iconfont') format('svg');
+	}
+	.iconfont {
+		font-family: "iconfont" !important;
+		font-size: 23px;
+		font-style: normal;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		
+	}
+
+
+	.footer{
+		padding:10px 0;
+		width:100%;
+		height:40px;
+		border-top:1px solid gray;
+		position:fixed;
+		bottom:0;
+		display: flex;
+		text-align:center;
+		background: white;
+		li{
+			flex:1;
+			a{
+				display: flex;
+				flex-direction: column;
+				height:40px;
+				justify-content: space-around;
+				font-size:14px;
+			}
+		}
+	}
+	.active{
+		color:red;
+	}
 </style>
